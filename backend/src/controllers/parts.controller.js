5322de248e29523453e362b868bfa1e42a9d5b7f
MMZ1969 <Mzimmermana2so@gmail.com>
2026-05-05 21:57:17 -0400

    Add phone to getPartById response

--- backend/src/controllers/parts.controller.js
+++ backend/src/controllers/parts.controller.js
@@ -63,6 +63,7 @@ export async function getPartById(req, res) {
             profilePhoto: true,
             role: true,
             repPoints: true,
+            phone: true,
           },
         },
       },
